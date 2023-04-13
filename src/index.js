0025a32e9a4efffa8069c93641d0f9eddea315ec
Joepolymath <joshuaajagbe96@gmail.com>
2023-04-13 16:30:43 +0100

    admin seeding

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const app = require('./setup/express.js');
 const logging = require('./setup/logging.js');
 const database = require('./setup/database.js');
+const seedAdmin = require('./seeding/index.js');
 
 logging();
 database();
+seedAdmin();
 
 module.exports = app;
--- src/seeding/index.js
+++ src/seeding/index.js
@@ -1,1 +1,33 @@
 const User = require('../models/user.model');
+
+const seedAdmin = async () => {
+  const foundAdmin = await User.find({ role: 'admin' });
+  if (foundAdmin.length < 1) {
+    const data = {
+      firstName: 'Church',
+      lastName: 'Admin',
+      email: 'admin@gmail.com',
+      phone: '08011223344',
+      password: 'testing',
+      address: 'F5 Police Estate Kubwa',
+      dateOfBirth: '26/08/1996',
+      occupation: 'Admin',
+      role: 'admin',
+      gender: 'male',
+      isMarried: false,
+      units: ['Choir'],
+      profileImg: 'image url here',
+    };
+
+    const createdAdmin = await User.create(data);
+    if (!createdAdmin) {
+      console.log('Unable to seed data');
+      return;
+    }
+    console.log('Admin Seeding Successful');
+    return;
+  }
+  return;
+};
+
+module.exports = seedAdmin;
